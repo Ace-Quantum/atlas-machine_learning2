39c17d5a8d64d783f30913e278c269dd26b9a5ba
root <root@DESKTOP-FADRK94>
2024-05-01 10:50:16 -0500

    documentation

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 """ A program made to determine a shape """
+
+
 def matrix_shape(matrix):
     if isinstance(matrix, list):
         return [len(matrix)] + matrix_shape(matrix[0])
