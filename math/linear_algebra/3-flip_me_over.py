ea01c36d5064a736f7378b2b646f69365f6d915a
root <root@DESKTOP-FADRK94>
2024-05-01 11:53:47 -0500

    Thank you Geeks4Geeks

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""This file is solely for the purpose of transposing matrixes"""
+
+def matrix_transpose(matrix):
+    """heck"""
+    
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    transposed = [[0 for _ in range(rows)] for _ in range(cols)]
+
+    for i in range(rows):
+        for j in range(cols):
+            transposed[j][i] = matrix[i][j]
+
+    return(transposed)
