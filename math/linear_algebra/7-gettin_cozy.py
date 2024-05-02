564c41b0f27217285ed373c0b020a1c87af67962
root <root@DESKTOP-FADRK94>
2024-05-02 10:11:06 -0500

    Check for dimensions

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-basic_matrix = __import__('6-howdy_partner').cat_arrays
+basic_matrix_cat = __import__('6-howdy_partner').cat_arrays
+size_matrix = __import__('2-size_me_please').matrix_shape
+
 
 def cat_matrices2D(mat1, mat2, axis=0):
 
@@ -9,10 +11,11 @@ def cat_matrices2D(mat1, mat2, axis=0):
 
     return_arr = [[0 for _ in range(cols)] for _ in range(rows)]
 
-
-    if axis == 0:
-        return (basic_matrix(mat1, mat2))
+    if len(size_matrix(mat1)) != len(size_matrix(mat2)):
+        return (None)
+    elif axis == 0:
+        return (basic_matrix_cat(mat1, mat2))
     else:
-        return_arr[0] = basic_matrix(mat1[0], mat2[0])
-        return_arr[1] = basic_matrix(mat1[1], mat2[1])
+        return_arr[0] = basic_matrix_cat(mat1[0], mat2[0])
+        return_arr[1] = basic_matrix_cat(mat1[1], mat2[1])
         return (return_arr)
