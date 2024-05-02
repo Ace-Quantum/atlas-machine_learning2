d2cfd12d22ee3a40e5c59a84015e2265b00ec68a
root <root@DESKTOP-FADRK94>
2024-05-02 09:10:12 -0500

    Trying with a new check

--- math/linear_algebra/tester.py
+++ math/linear_algebra/tester.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-cat_arrays = __import__('6-howdy_partner').cat_arrays
+add_matrices2D = __import__('5-across_the_planes').add_matrices2D
 
-arr1 = [1, 2, 3, 4, 5]
-arr2 = [6, 7, 8]
-print(cat_arrays(arr1, arr2))
-print(arr1)
-print(arr2)
+mat1 = [[1, 2], [3, 4]]
+mat2 = [[5, 6], [7, 8]]
+print(add_matrices2D(mat1, mat2))
+print(mat1)
+print(mat2)
+print(add_matrices2D(mat1, [[1, 2, 3], [4, 5, 6]]))
