f8048cfdd4eead6d180d92b38df3bcd564e4b547
root <root@DESKTOP-FADRK94>
2024-05-07 11:33:16 -0500

    idk why my terminal is acting this way but I'm not happy about it

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Here's some more docs!!!"""
+
+
+def np_shape(matrix):
+    """And some more"""
+    return matrix.shape
--- math/linear_algebra/tester.py
+++ math/linear_algebra/tester.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-mat_mul = __import__('8-ridin_bareback').mat_mul
+import numpy as np
+np_shape = __import__('10-ill_use_my_scale').np_shape
 
-mat1 = [[3, 5],
-        [8, 7],
-        [6, 2]]
-
-mat2 = [[3, 5, 1, 7],
-        [6, 8, 4, 2]]
-
-mat_mul(mat1, mat2)
+mat1 = np.array([1, 2, 3, 4, 5, 6])
+mat2 = np.array([])
+mat3 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
+                 [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]])
+print(np_shape(mat1))
+print(np_shape(mat2))
+print(np_shape(mat3))
