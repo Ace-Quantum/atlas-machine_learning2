8e1c3cc1093e3851b94351e3098e654a203c9c3a
root <root@DESKTOP-FADRK94>
2024-05-07 12:51:34 -0500

    Every point counts I guess

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -2,12 +2,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+"""Here's some documentation"""
+
+
 def line():
+    """And some more despite this being code from the project itself"""
 
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
     plt.axis([0, 10, -50, 1050])
-    plt.plot(y, color='red')
+    plt.plot(y, color="red")
 
     plt.show()
