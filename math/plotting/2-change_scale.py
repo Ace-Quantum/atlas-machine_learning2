15030627076fb5c6564f37fed8404fcdcb2e72fe
root <root@DESKTOP-FADRK94>
2024-05-07 13:25:32 -0500

    documentation

--- math/plotting/2-change_scale.py
+++ math/plotting/2-change_scale.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+"""Documentation goes here"""
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def change_scale():
+    """And here as well"""
     x = np.arange(0, 28651, 5730)
     r = np.log(0.5)
     t = 5730
