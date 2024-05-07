e3aaa5e0a547b601252906641b8cb472d56f026e
root <root@DESKTOP-FADRK94>
2024-05-07 10:05:23 -0500

    A little frustrated I wasted so much time on this

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+size_check = __import__('2-size_me_please').matrix_shape
+
+def mat_mul(mat1, mat2):
+
+    rows = size_check(mat1)[0]
+    cols = size_check(mat2)[1]
+
+    return_arr = [[0 for _ in range(cols)] for _ in range(rows)]
+
+    # print("Size mat1: " + str(size_check(mat1)))
+    # print("Size mat2: " + str(size_check(mat2)))
+    # print("Size return_arr: " + str(size_check(return_arr)))
+
+    if size_check(mat1)[1] != size_check(mat2)[0]:
+        return (None)
+    
+    for i in range(rows):
+        for j in range(cols):
+            for k in range(len(mat2)):
+                return_arr[i][j] += mat1[i][k] * mat2[k][j]
+                # print(return_arr[i][j])
+
+    # print(return_arr)
+    return (return_arr)
--- math/linear_algebra/tester.py
+++ math/linear_algebra/tester.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
 
-cat_matrices2D = __import__('7-gettin_cozy').cat_matrices2D
+mat_mul = __import__('8-ridin_bareback').mat_mul
 
-mat1 = [[1, 2], [3, 4]]
-mat2 = [[[5, 6], [5, 7]], [[3, 4], [5, 9]]]
-mat3 = [[7], [8]]
-mat4 = cat_matrices2D(mat1, mat2)
-# mat5 = cat_matrices2D(mat1, mat3, axis=1)
-print(mat4)
-# print(mat5)
-# mat1[0] = [9, 10]
-# mat1[1].append(5)
-# print(mat1)
-# print(mat4)
-# print(mat5)
+mat1 = [[3, 5],
+        [8, 7],
+        [6, 2]]
+
+mat2 = [[3, 5, 1, 7],
+        [6, 8, 4, 2]]
+
+mat_mul(mat1, mat2)
