6535d957e35caaa1b5237f0466d340a6e8b27e92
root <root@DESKTOP-FADRK94>
2024-05-07 10:18:24 -0500

    Gonna scrape a few points the rest of this pomo

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -6,14 +6,6 @@ size_matrix = __import__("2-size_me_please").matrix_shape
 
 def cat_matrices2D(mat1, mat2, axis=0):
 
-    # if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
-    # return (None)
-
-    # if len(size_matrix(mat1)) != len(size_matrix(mat2)):
-    # return (None)
-    # print ("len of mat1[0]: " + str(len(mat1[0])))
-    # print ("len of mat2[0]: " + str(len(mat2[0])))
-
     if axis == 0 and len(mat1[0]) != len(mat2[0]):
         return None
     if axis == 1 and len(mat1) != len(mat2):
@@ -26,6 +18,8 @@ def cat_matrices2D(mat1, mat2, axis=0):
     if axis == 0:
         return basic_matrix_cat(mat1, mat2)
     else:
-        return_arr[0] = basic_matrix_cat(mat1[0], mat2[0])
-        return_arr[1] = basic_matrix_cat(mat1[1], mat2[1])
+        return_arr = []
+        for i in range(len(mat1)):
+            row = basic_matrix_cat(mat1[i], mat2[i])
+            return_arr.append(row)
         return return_arr
