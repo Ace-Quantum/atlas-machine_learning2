e0e5bc9a128082bf51c2f8c84115410f293cf3e9
root <root@DESKTOP-FADRK94>
2024-05-15 06:47:10 -0500

    okay cool so reciprocals are something I definitely don't understand

---
+++ math/probability/exponential.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Here's some documentation"""
+
+class Exponential:
+    """more documentation"""
+
+    def __init__(self, data=None, lambtha=1.):
+        """More!!! More pictures of spiderman!!!"""
+        self.lambtha = float(lambtha)
+
+        if data is None:
+            if lambtha < 1:
+                raise ValueError("lambtha must be a positive value")
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                self.lambtha = len(data) / sum(data)
