a6686ce21817cea657d702fa86989212ef773c2e
root <root@DESKTOP-FADRK94>
2024-05-14 20:28:58 -0500

    documentation/formatting

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
+"""Here's some documentation"""
+
 
 class Poisson:
-    def __init__(self, data=None, lambtha=1.):
+    """And more? Wow I never would have guessed"""
 
+    def __init__(self, data=None, lambtha=1.0):
+        """Woah! More documentation!!"""
         self.lambtha = float(lambtha)
 
         if data == None:
             if lambtha < 1:
-                raise ValueError('lambtha must be a positive value')
+                raise ValueError("lambtha must be a positive value")
         else:
             if type(data) != list:
-                raise TypeError('data must be a list')
+                raise TypeError("data must be a list")
             elif len(data) < 2:
-                raise ValueError('data must contain multiple values')
+                raise ValueError("data must contain multiple values")
             else:
                 self.lambtha = sum(data) / len(data)
