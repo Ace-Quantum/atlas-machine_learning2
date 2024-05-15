c15dfb93224916f1eae3eece25b7ab64e3eae39e
root <root@DESKTOP-FADRK94>
2024-05-15 06:47:51 -0500

    formatting

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """Here's some documentation"""
 
+
 class Exponential:
     """more documentation"""
 
-    def __init__(self, data=None, lambtha=1.):
+    def __init__(self, data=None, lambtha=1.0):
         """More!!! More pictures of spiderman!!!"""
         self.lambtha = float(lambtha)
 
