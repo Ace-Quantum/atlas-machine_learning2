dd7ab14620eaac9943c40c40579aa950d73055bf
root <root@DESKTOP-FADRK94>
2024-05-24 14:57:26 -0500

    Took me a second to realize they were wanting me to get that error

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import numpy as np
+
+"""Documentation is here"""
+
+
+class Neuron:
+    """Idk what to put here but it's kinda cool
+    that we're building a fancy calculator
+    that can recognize digits"""
+
+    def __init__(self, nx):
+        """Oh look more documentation"""
+
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        
+        self.nx = nx
+
+    @property
+    def W(self):
+        self.__W = np.random.normal(size=(1, self.nx))
+        return self.__W
+    
+    @property
+    def b(self):
+        if not hasattr(self, '__b'):
+            self.__b = 0
+        return self.__b
+    
+    @property
+    def A(self):
+        if not hasattr(self, '__A'):
+            self.__A = 0
+        return self.__A
