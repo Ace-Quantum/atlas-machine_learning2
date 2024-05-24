790750c458b5e2bbeda836c80606ea96010b9329
root <root@DESKTOP-FADRK94>
2024-05-24 14:34:21 -0500

    thank you Sean

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -13,6 +13,6 @@ class Neuron:
         if nx < 1:
             raise ValueError("nx must be a positive integer")
         
-        self.W = np.random.normal(0, .1, nx)
+        self.W = np.random.normal(size=(1, nx))
         self.b = 0
         self.A  = 0
