3ba4eaeba9e7baf816a8198003a371611b3339b5
root <root@DESKTOP-FADRK94>
2024-05-24 14:35:28 -0500

    documentation

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import numpy as np
-
+"""Documentation is here"""
 
 class Neuron:
     """Idk what to put here but it's kinda cool
@@ -8,11 +8,12 @@ class Neuron:
     that can recognize digits"""
 
     def __init__(self, nx):
+        """Oh look more documentation"""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        
+
         self.W = np.random.normal(size=(1, nx))
         self.b = 0
-        self.A  = 0
+        self.A = 0
