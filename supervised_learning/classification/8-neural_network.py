c55bb1be7755f996bcf626e5a3d301b46352fbc0
root <root@DESKTOP-FADRK94>
2024-05-29 12:36:21 -0500

    That is a lot of data tbh

---
+++ supervised_learning/classification/8-neural_network.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+"""Building the network itself"""
+
+import numpy as np
+
+"""Idk I'm just putting documentation everywhere"""
+
+class NeuralNetwork:
+    """One day AI will take over the world. 
+    Let's hope they're nice about it"""
+    def __init__(self, nx, nodes):
+        """crickets"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.W1 = np.random.normal(size=(1, nx))
+        self.b1 = 0
+        self.A1 = 0
+        self.W2 = np.random.normal(size=(1, nx))
+        self.b2 = 0
+        self.A2 = 0
