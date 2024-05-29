07eef7c16331c24fc23434e3d821e98c80d8fa8b
root <root@DESKTOP-FADRK94>
2024-05-29 11:27:29 -0500

    Time to spam the checker again

--- supervised_learning/classification/6-neuron.py
+++ supervised_learning/classification/6-neuron.py
@@ -85,10 +85,15 @@ class Neuron:
         self.__b -= alpha * db
 
     def train(self, X, Y, iterations=5000, alpha=0.05):
-        if not isinstance(alpha, float):
-            raise TypeError("xyz")
+        if not isinstance(iterations, int):
+            raise TypeError("iterations must be an integer")
+        if iterations < 0:
+            raise ValueError("iterations must be a positive integer")
+        
+        if not isinstance (alpha, float):
+            raise TypeError("alpha must be a float")
         if alpha < 0:
-            raise ValueError("x")
+            raise ValueError("alpha must be positive")
         
         for epoch in range(iterations):
             A = self.forward_prop(X)
