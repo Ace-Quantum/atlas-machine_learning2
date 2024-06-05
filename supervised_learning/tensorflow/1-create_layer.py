c86c1dd8943d05f89a976d1dbcedbf36fa742562
root <root@DESKTOP-FADRK94>
2024-06-05 17:40:46 -0500

    heck

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -3,9 +3,6 @@
 import tensorflow.compat.v1 as tf
 tf.disable_eager_execution()
 
-# create_placeholders = __import__('0-create_placeholders').create_placeholders
-# create_layer = __import__('1-create_layer').create_layer
-
 def create_layer(prev, n, activation):
     initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
     layer = tf.layers.Dense(units=n, activation=activation, kernel_initializer=initializer, name="layer")
