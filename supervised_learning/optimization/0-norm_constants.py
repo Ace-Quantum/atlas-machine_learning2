fad890a9e79347789b8b4b29f9a4143a41f8dcf3
root <root@DESKTOP-FADRK94>
2024-06-09 20:14:07 -0500

    Keyboard shorctcut ftw

--- supervised_learning/optimization/0-norm_constants.py
+++ supervised_learning/optimization/0-norm_constants.py
@@ -1,24 +1,14 @@
 #!/usr/bin/env python3
 
+"""Behold, documentation"""
+
 import numpy as np
 
-def normalization_constants(X):
 
-    # norm_X = (X - np.mean(X, axis=0)) / np.std(X, axis=0)
+def normalization_constants(X):
+    """I should at some point put effort into documentation"""
 
     mean_vals = np.mean(X, axis=0)
     standard_vals = np.std(X, axis=0)
 
     return mean_vals, standard_vals
-
-
-    # norm_arr = []
-    # for i in X:
-    #     temp = i / X.max()
-    #     norm_arr.append(temp)
-
-
-
-    # diff_arr = np.max(X) - np.min(X)
-
-    # norm_array = ((X - X.min()) / diff_arr)
