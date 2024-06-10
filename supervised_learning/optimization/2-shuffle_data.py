eb25f76948f2d4307aef50db28e8392d005cba8f
root <root@DESKTOP-FADRK94>
2024-06-09 20:58:38 -0500

    Missed the i

--- supervised_learning/optimization/2-shuffle_data.py
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -1,8 +1,10 @@
-#!/usr/bn/env python3
+#!/usr/bin/env python3
+"""Documentation"""
 
 import numpy as np
 
 def shuffle_data(X, Y):
+    """Documentation"""
     assert len(X) == len(Y)
     p = np.random.permutation(len(X))
     return X[p], Y[p]
