a4d01548a732affec45beab4ac2bf1ecd2a865af
root <root@DESKTOP-FADRK94>
2024-06-09 20:08:02 -0500

    This is what I get for thinking

--- supervised_learning/optimization/0-norm_constants.py
+++ supervised_learning/optimization/0-norm_constants.py
@@ -3,14 +3,22 @@
 import numpy as np
 
 def normalization_constants(X):
-    norm_array = []
-    diff_arr = np.amax(X) - np.amin(X)
 
-    for i in X:
-        temp = (i - np.min(X))/diff_arr
-        norm_array.append(temp)
+    # norm_X = (X - np.mean(X, axis=0)) / np.std(X, axis=0)
 
-    mean_vals = np.mean(norm_array, axis=0)
-    standard_vals = np.std(norm_array, axis=0)
+    mean_vals = np.mean(X, axis=0)
+    standard_vals = np.std(X, axis=0)
 
     return mean_vals, standard_vals
+
+
+    # norm_arr = []
+    # for i in X:
+    #     temp = i / X.max()
+    #     norm_arr.append(temp)
+
+
+
+    # diff_arr = np.max(X) - np.min(X)
+
+    # norm_array = ((X - X.min()) / diff_arr)
