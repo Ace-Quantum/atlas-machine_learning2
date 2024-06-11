d49ce7f0c5897f3d210494df0431b51c2a0fdacd
root <root@DESKTOP-FADRK94>
2024-06-10 21:27:19 -0500

    documentation and formatting

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+"""Documentation"""
 
-shuffle_data = __import__('2-shuffle_data').shuffle_data
+shuffle_data = __import__("2-shuffle_data").shuffle_data
+
 
 def create_mini_batches(X, Y, batch_size):
+    """Documentation"""
     mini_batches = []
     # data = np.haystack(X, Y)
     # np.random.shuffle(data)
@@ -11,7 +14,7 @@ def create_mini_batches(X, Y, batch_size):
 
     X_shuf, Y_shuf = shuffle_data(X, Y)
 
-    n_minibatches =  len(X_shuf) // batch_size
+    n_minibatches = len(X_shuf) // batch_size
 
     i = 0
 
@@ -19,7 +22,6 @@ def create_mini_batches(X, Y, batch_size):
         start = i * batch_size
         end = start + batch_size
         mini_batches.append((X_shuf[start:end], Y_shuf[start:end]))
-
 
     if len(X_shuf) % batch_size != 0:
         last_batch_size = len(X_shuf) - (n_minibatches * batch_size)
