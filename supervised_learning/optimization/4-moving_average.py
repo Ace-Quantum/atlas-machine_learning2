dcb31048b35b8c3f014cc82708985dd0df1f7603
root <root@DESKTOP-FADRK94>
2024-06-12 14:49:53 -0500

    f i n a l l y

--- supervised_learning/optimization/4-moving_average.py
+++ supervised_learning/optimization/4-moving_average.py
@@ -7,21 +7,18 @@ def moving_average(data, beta):
 
     moving_averages = []
 
-    float_beta = float(beta)
+    v_t = 0
 
     if len(data) == 0:
         return moving_averages
     
-    full_weight = sum([(i+1)**(-float_beta) for i in range(1, len(data)+1)])
-
-    # moving_averages.append(data[0])
-
-    # exp_weighted_avg = 0
+    # full_weight = sum([(i+1)**(-float_beta) for i in range(len(data))])
 
     for i in range(len(data)):
-        weight_sum = sum([(i+1-j)**(-float_beta) * data[j] for j in range(i + 1)])
-        moving_average = weight_sum / full_weight
-        moving_averages.append(moving_average)
+        v_t = beta * v_t + (1 - beta) * data[i]
+        # weight_sum = sum([(i+1-j)**(-float_beta) * data[j] for j in range(int(beta), min(i+1, len(data) + 1))])
+        # moving_average = weight_sum / full_weight
+        moving_averages.append(v_t / (1 - beta**(i+1)))
         
 
     return moving_averages
