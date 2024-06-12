a89d4cb1b39d0eff6e1a7f2d0915aab6818c5688
root <root@DESKTOP-FADRK94>
2024-06-12 17:52:37 -0500

    keyboard shortcuts are my lifeblood

--- supervised_learning/optimization/5-momentum.py
+++ supervised_learning/optimization/5-momentum.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
+"""documentation"""
+
+
 def update_variables_momentum(alpha, beta1, var, grad, v):
+    """documentation"""
     v_new = beta1 * v + (1 - beta1) * grad
     var_new = var - alpha * v_new
     return var_new, v_new
