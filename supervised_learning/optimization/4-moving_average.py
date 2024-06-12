4238441d845918451375c5487427b5e3eea097d5
root <root@DESKTOP-FADRK94>
2024-06-12 00:36:29 -0500

    I'm not even testing locally anymore tbh

--- supervised_learning/optimization/4-moving_average.py
+++ supervised_learning/optimization/4-moving_average.py
@@ -24,7 +24,7 @@ def moving_average(data, beta):
 
     n = len(data)
     d = len(moving_averages) -1
-    bias_correction = (n - 1) / n
+    bias_correction = n /d
     biased_moving_averages = [ma * bc for ma, bc in zip(moving_averages, [bias_correction] * len(moving_averages))]
 
     return biased_moving_averages
