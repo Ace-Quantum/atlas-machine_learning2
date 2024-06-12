1956bcf1f4a189c10089daefa591beaeeb34861a
root <root@DESKTOP-FADRK94>
2024-06-12 00:24:50 -0500

    Forgot to initialize my variable

--- supervised_learning/optimization/4-moving_average.py
+++ supervised_learning/optimization/4-moving_average.py
@@ -9,14 +9,14 @@ def moving_average(data, beta):
 
     moving_averages = []
 
-    if len(data) > 0:
-        moving_averages.append(data[0])
-    else:
+    if len(data) == 0:
         return moving_averages
     
     # n = sum((x - moving_averages[-1]) **2 for x in data)
     # d = len(data)
     # bias_correction_tem = n / d
+
+    moving_averages.append(data[0])
 
     for i in range(1, len(data)):
         moving_average = alpha * data[i] + beta * moving_averages[-1]
