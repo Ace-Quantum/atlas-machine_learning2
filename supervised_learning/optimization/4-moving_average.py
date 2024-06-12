39bd02767973631d27e6c13318a364ef584f6a6f
root <root@DESKTOP-FADRK94>
2024-06-12 14:51:07 -0500

    formatting

--- supervised_learning/optimization/4-moving_average.py
+++ supervised_learning/optimization/4-moving_average.py
@@ -2,6 +2,7 @@
 
 """documentation here"""
 
+
 def moving_average(data, beta):
     """documentation here"""
 
@@ -11,14 +12,9 @@ def moving_average(data, beta):
 
     if len(data) == 0:
         return moving_averages
-    
-    # full_weight = sum([(i+1)**(-float_beta) for i in range(len(data))])
 
     for i in range(len(data)):
         v_t = beta * v_t + (1 - beta) * data[i]
-        # weight_sum = sum([(i+1-j)**(-float_beta) * data[j] for j in range(int(beta), min(i+1, len(data) + 1))])
-        # moving_average = weight_sum / full_weight
-        moving_averages.append(v_t / (1 - beta**(i+1)))
-        
+        moving_averages.append(v_t / (1 - beta ** (i + 1)))
 
     return moving_averages
