63a391513313af7fcf709e094a8caba024ccfafc
root <root@DESKTOP-FADRK94>
2024-06-12 18:46:35 -0500

    I have no idea what's happening here

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -5,9 +5,6 @@ import tensorflow as tf
 def create_momentum_op(alpha, beta1):
     global_step = tf.Variable(0)
 
-    optimizer = tf.train.MomentumOptimizer(alpha, beta1, use_nesterov=True)
+    optimizer = tf.train.MomentumOptimizer(alpha, beta1, use_nesterov=True).minimize(tf.losses.mean_squared_error(), global_step=global_step)
 
-    loss_func = tf.losses.mean_squared_error()
-    train_op = optimizer.minimize(loss_func, global_step=global_step)
-
-    return train_op
+    return optimizer
