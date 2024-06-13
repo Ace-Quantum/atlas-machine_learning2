56f2a7ff0bc036bc181a48940758337d4307f2d5
root <root@DESKTOP-FADRK94>
2024-06-12 20:49:44 -0500

    Let's see if this is the right bias

--- supervised_learning/optimization/9-Adam.py
+++ supervised_learning/optimization/9-Adam.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
 def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
-    v_corrected = v / (1 - beta1 ** t)
-
-    s_corrected = s / (1 - beta2 ** t)
-
-    var_updated = var - alpha * v_corrected / ((s_corrected ** 0.5) + epsilon)
 
     v = beta1 * v + (1 - beta1) * grad
-    s = beta2 * s + (1 - beta2) * grad ** 2
+    s = beta2 * s + (1 - beta2) * (grad ** 2)
 
-    return var_updated, v, s
+    v_cor = v / (1 - beta1 ** t)
+    s_cor = s / (1 - beta2 ** t)
+
+    var -= alpha * v_cor / ((s_cor ** 0.5) + epsilon)
+
+    return var, v, s
