03533b863dfd9a2c11ddbe35a230970e65563407
root <root@DESKTOP-FADRK94>
2024-06-12 19:03:57 -0500

    docuformatting

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-
+"""Documentation"""
 import tensorflow as tf
 
-def create_momentum_op(alpha, beta1):
-    # global_step = tf.Variable(0)
 
-    optimizer = tf.keras.optimizers.SGD(
-        learning_rate=alpha, 
-        momentum=beta1)
+def create_momentum_op(alpha, beta1):
+    """Documentation"""
+
+    optimizer = tf.keras.optimizers.SGD(learning_rate=alpha, momentum=beta1)
 
     return optimizer
