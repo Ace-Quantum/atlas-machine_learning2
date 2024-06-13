9855be70a6a4c8a7c8c042d39ac7bcdd7ef94f3b
root <root@DESKTOP-FADRK94>
2024-06-12 19:57:49 -0500

    I'm gonna take the L on that one

--- supervised_learning/optimization/7-RMSProp.py
+++ supervised_learning/optimization/7-RMSProp.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+"""Documentation"""
+
 
 def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
-    s_new = beta2 * s + (1 - beta2) * (grad ** 2)
+    """Documentation"""
+    s_new = beta2 * s + (1 - beta2) * (grad**2)
 
-    var_new = var - alpha * grad / (s_new + epsilon)**0.5
+    var_new = var - alpha * grad / (s_new + epsilon) ** 0.5
 
     return var_new, s_new
