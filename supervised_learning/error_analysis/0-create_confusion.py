e9099b7889c37ad9eaa5efc9a90a1057a7f97eb9
root <root@DESKTOP-FADRK94>
2024-06-16 20:22:30 -0500

    formatting and readme and whatnot

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -8,7 +8,8 @@ def create_confusion_matrix(labels, logits):
     true_labels = labels.argmax(axis=1)
     predicted_labels = logits.argmax(axis=1)
 
-    con_mat = np.zeros((len(np.unique(true_labels)), len(np.unique(predicted_labels))))
+    con_mat = np.zeros((len(np.unique(true_labels)
+                            ), len(np.unique(predicted_labels))))
 
     for i, true_label in enumerate(true_labels):
         con_mat[true_label, predicted_labels[i]] += 1
