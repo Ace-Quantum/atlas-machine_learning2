bc70b23a22396c095a9f5a91ec298738b9d0e02e
root <root@DESKTOP-FADRK94>
2024-06-19 17:10:40 -0500

    Ok let's see where these get us

--- supervised_learning/error_analysis/2-precision.py
+++ supervised_learning/error_analysis/2-precision.py
@@ -15,4 +15,4 @@ def precision(confusion):
         sens = true_pos / (true_pos + false_neg) if false_neg > 0 else 1
         sensetivity_return.append(round(sens, 8))
 
-    return sensetivity_return
+    return np.array(sensetivity_return)
--- supervised_learning/error_analysis/3-specificity.py
+++ supervised_learning/error_analysis/3-specificity.py
@@ -8,13 +8,28 @@ def specificity(confusion):
     """documentation"""
     specificity_return = []
     num_classes = confusion.shape[0]
-    # print("Num classes" + str(num_classes))
+    true_pos = np.diag(confusion)
+    true_negs = np.diag(confusion)
+    false_poses = np.array([np.sum(row) - tp for tp, row in zip(true_pos, confusion.T)])
 
     for i in range(num_classes):
-        true_neg = np.diag(confusion)
-        false_pos = np.sum(confusion, axis=0) - true_pos
+        true_neg = true_negs[i]
+        false_pos = false_poses[i]
 
-        specs = true_neg / (false_pos + false_neg) if false_neg > 0 else 1
-        specificity_return.append(round(specs, 8))
+        spec = true_neg / (true_neg + false_pos) if false_pos > 0 else 1
+        specificity_return.append(spec)
 
     return specificity_return
+
+
+
+    # # print("Num classes" + str(num_classes))
+
+    # for i in range(num_classes):
+    #     true_neg = np.diag(confusion)
+    #     false_pos = np.sum(confusion, axis=0) - true_pos
+
+    #     specs = true_neg / (false_pos + false_neg) if false_neg > 0 else 1
+    #     specificity_return.append(round(specs, 8))
+
+    # return specificity_return
