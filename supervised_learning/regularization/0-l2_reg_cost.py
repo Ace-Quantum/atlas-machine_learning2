88304dd7ace23896498ab838862d8078ce134589
root <root@DESKTOP-FADRK94>
2024-06-20 10:12:21 -0500

    Idk how to run pystyle so let's hope this works

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -4,5 +4,19 @@ import numpy as np
 
 
 def l2_reg_cost(cost, lambtha, weights, L, m):
-    """And some more documentation"""
-    return 0
+    """Variables:
+    cost - cost of the network without L2 regularization
+    lambtha - regularization parameter
+    weights - dictionary of the weights and biases (numpy.ndarrays) of the neural network
+    L - number of layers in the neural network
+    m - number of data points used
+    Returns: the cost of the network accounting for L2 regularization"""
+
+    reg_cost = 0
+
+    for layer_name, weight in weights.items():
+        reg_cost += np.sum(np.square(weight))
+
+    reg_term = lambtha / (2 * m) * reg_cost
+
+    return cost + reg_term
