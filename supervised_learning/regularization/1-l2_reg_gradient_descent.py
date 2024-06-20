50f1e4af6d011427926c7c95014ada4b3f1fcacf
root <root@DESKTOP-FADRK94>
2024-06-19 19:29:23 -0500

    skeleton

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -3,6 +3,6 @@
 import numpy as np
 
 
-def def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
+def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """And some more documentation"""
     return 0
