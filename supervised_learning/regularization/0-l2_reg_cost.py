462e37f7cd38a06c41f06007143a0521b50bee44
root <root@DESKTOP-FADRK94>
2024-06-20 11:57:22 -0500

    pycode

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -7,7 +7,8 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     """Variables:
     cost - cost of the network without L2 regularization
     lambtha - regularization parameter
-    weights - dictionary of the weights and biases (numpy.ndarrays) of the neural network
+    weights - dictionary of the weights and biases (
+        numpy.ndarrays) of the neural network
     L - number of layers in the neural network
     m - number of data points used
     Returns: the cost of the network accounting for L2 regularization"""
