e01888b968dab2eb8cf7df548515a4dbf2033b7e
root <root@DESKTOP-FADRK94>
2024-06-20 11:32:40 -0500

    Now to figure out why that line works

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -14,10 +14,8 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
 
     reg_cost = 0
 
-    num_weights_and_biases = len(weights) // 2
-
-    weight_matrices = weights[:num_weights_and_biases].reshape((L, -1))
-    bias_vectors = weights[num_weights_and_biases:].reshape((L, -m))
+    weight_matrices = [weights[f'W{i+1}'] for i in range(L)]
+    bias_vectors = [weights.get(f'b{i+1}', np.zeros_like(weight_matrices[i][0])) for i in range(L)]
 
     for i in range(L):
         reg_cost += np.sum(np.square(weight_matrices[i])) + np.sum(
