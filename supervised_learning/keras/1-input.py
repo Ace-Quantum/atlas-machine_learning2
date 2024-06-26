14891d3e698ec09482b1273978ffc006315acb0c
root <root@DESKTOP-FADRK94>
2024-06-26 14:58:38 -0500

    Maybe this?

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -12,7 +12,8 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
 
     for i, num_nodes in enumerate(layers):
         x = K.layers.Dense(num_nodes, kernel_regularizer=K.regularizers.l2(lambtha))(x)
-        x = activations[i](x)
+        # x = activations[i](x)
+        x = activations[i]
 
         if i < len(layers) - 1:
             x = K.layers.Dropout(keep_prob)(x)
