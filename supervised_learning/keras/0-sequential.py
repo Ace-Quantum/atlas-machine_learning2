bc7101eaf5e0106aca88c957e0cd2d6b3de9d01a
Ace <AceQuantumBit@gmail.com>
2024-06-26 13:11:32 -0500

    Let's get to it

---
+++ supervised_learning/keras/0-sequential.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import tensorflow.keras as K
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    model = K.models.Sequential()
+
+    for i in range(len(layers)):
+        model.add(Dense(units=layers[i] + 1, activation=activations[i], kernel_regularizer=k.regularizers.12(lambtha)))
+        if i < len(layers) - 1:
+            model.add(Dropout(rate=keep_prob))
+
+            model.compile(optimizer='adam', loss='mean_squared_error')
+
+            return model
