79a2a09ff1af5dfc7d3b632c73fae60922246fa6
root <root@DESKTOP-FADRK94>
2024-06-26 13:31:11 -0500

    pycode

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
+"""Documentation"""
 
 import tensorflow.keras as K
 
+
 def build_model(nx, layers, activations, lambtha, keep_prob):
+    """Documentation"""
     model = K.models.Sequential()
 
     for i in range(len(layers)):
-        model.add(K.layers.Dense(units=layers[i] + 1, activation=activations[i], kernel_regularizer=K.regularizers.l2(lambtha)))
+        model.add(
+            K.layers.Dense(
+                units=layers[i] + 1,
+                activation=activations[i],
+                kernel_regularizer=K.regularizers.l2(lambtha),
+            )
+        )
         if i < len(layers) - 1:
             model.add(K.layers.Dropout(rate=keep_prob))
 
-    model.compile(optimizer='adam', loss='mean_squared_error')
+    model.compile(optimizer="adam", loss="mean_squared_error")
 
     return model
