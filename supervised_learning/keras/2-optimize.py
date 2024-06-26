e529794b68cc14df8b161edff25eb31ae0ee9a13
root <root@DESKTOP-FADRK94>
2024-06-26 15:25:43 -0500

    Oh no my music stopped

---
+++ supervised_learning/keras/2-optimize.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""Documentation"""
+
+import tensorflow.keras as K
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    """Documentation"""
+
+    adam_optomizer = K.optimizers.Adam(lr=alpha, beta_1=beta1, beta_2=beta2)
+
+    network.compile(
+        loss="categorical_crossentropy", 
+        optimizer=adam_optomizer, 
+        metrics=["accuracy"]
+    )
