578e60695eed34702ead039b1ca3e01a8ad8c279
Ace <AceQuantumBit@gmail.com>
2024-06-26 13:21:06 -0500

    fixed usage of functions

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -6,9 +6,9 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     model = K.models.Sequential()
 
     for i in range(len(layers)):
-        model.add(Dense(units=layers[i] + 1, activation=activations[i], kernel_regularizer=K.regularizers.l2(lambtha)))
+        model.add(K.layers.Dense(units=layers[i] + 1, activation=activations[i], kernel_regularizer=K.regularizers.l2(lambtha)))
         if i < len(layers) - 1:
-            model.add(Dropout(rate=keep_prob))
+            model.add(K.layers.Dropout(rate=keep_prob))
 
     model.compile(optimizer='adam', loss='mean_squared_error')
 
