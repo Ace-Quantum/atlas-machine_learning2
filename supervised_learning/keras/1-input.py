740db16f6799627b622a130b8e002a8e16a9797c
root <root@DESKTOP-FADRK94>
2024-06-26 18:11:53 -0500

    idk man I just work here

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -6,22 +6,23 @@ import tensorflow.keras as K
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """Documentation"""
-    inputs = K.Input(shape=(nx,))
+    inputs = K.layers.Input(shape=(nx,))
 
     x = inputs
 
     for i, num_nodes in enumerate(layers):
-        x = K.layers.Dense(
-            num_nodes,
-            kernel_regularizer=K.regularizers.l2(lambtha),
-            activation=activations[i],
-        )(drop)
+        x = K.layers.Dense(num_nodes, kernel_regularizer=K.regularizers.l2(lambtha))(x)
+        x = activations[i](x)
 
-        # if i < len(layers) - 1:
-        #     x = K.layers.Dropout(keep_prob)(x)
+        if i < len(layers) - 1:
+            x = K.layers.Dropout(keep_prob)(x)
 
-    # outputs = K.layers.Dense(layers[-1], activation="softmax")(x)
+    outputs = K.layers.Dense(layers[-1], activation="softmax")(x)
 
-    model = K.models.Model(inputs=inputs, outputs=x)
+    model = K.models.Model(inputs=inputs, outputs=outputs)
+
+    model.compile(
+        optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"]
+    )
 
     return model
