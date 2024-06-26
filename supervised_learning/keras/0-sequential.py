70c36821263b742b28fdab5d007ed6e2ca12b6c5
Ace <AceQuantumBit@gmail.com>
2024-06-26 13:17:43 -0500

    l2 and 12 are too close for my liking.

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -6,7 +6,7 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     model = K.models.Sequential()
 
     for i in range(len(layers)):
-        model.add(Dense(units=layers[i] + 1, activation=activations[i], kernel_regularizer=K.regularizers.12(lambtha)))
+        model.add(Dense(units=layers[i] + 1, activation=activations[i], kernel_regularizer=K.regularizers.l2(lambtha)))
         if i < len(layers) - 1:
             model.add(Dropout(rate=keep_prob))
 
