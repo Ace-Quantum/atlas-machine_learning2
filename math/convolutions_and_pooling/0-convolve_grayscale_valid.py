85f3b7c1c49f1c26ea428280c687ce7d52883267
root <root@DESKTOP-FADRK94>
2024-06-26 21:33:36 -0500

    task 0 test 1

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -7,3 +7,18 @@ import numpy as np
 
 def convolve_grayscale_valid(images, kernel):
     """Documentation"""
+
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+
+    out_h = h - kh + 1
+    out_w = w - kw + 1
+
+    output = np.zeros(m, out_h, out_w)
+
+    for i in range(m):
+        for y in range(out_h):
+            for x in range(out_w):
+                output[i, y, x] = np.sum(images[i, y:y+kh, x:x+kw] * kernel)
+
+    return output
