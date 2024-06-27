68e835cb9a41bbb42cf29f6f1c8f826a4f0548cf
root <root@DESKTOP-FADRK94>
2024-06-27 16:24:10 -0500

    let's see

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -11,8 +11,8 @@ def convolve_grayscale_same(images, kernel):
     m, h, w = images.shape
     kh, kw = kernel.shape
 
-    pad_h = (kh - 1) // 2
-    pad_w = (kw - 1) // 2
+    pad_h = (kh - 1) // 2 if kh % 2 == 1 else kh // 2
+    pad_w = (kw - 1) // 2 if kw % 2 == 1 else kw // 2
 
     pad_images = np.pad(images, ((0, 0), (pad_h, pad_h), (
         pad_w, pad_w)), mode='constant')
