380b33a57c1576086f035cc3da8292aa0caca896
root <root@DESKTOP-FADRK94>
2024-06-27 10:18:22 -0500

    Oh Python

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -3,25 +3,42 @@
 
 import tensorflow.keras as K
 
+# import tensorflow as tf
+
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """Documentation"""
-    shape = (None, nx)
-    model = K.models.Sequential([
-        K.layers.Input(shape=shape[1:])
-    ])
+    # shape = (None, nx
+    model = K.models.Sequential()
 
-    for i in range(len(layers)):
+    # l2 = regularizer l2=lambtha
+    l2 = K.regularizers.l2(lambtha)
+
+    # define first layer as before, but with input shape
+    model.add(
+        K.layers.Dense(
+            units=layers[0],
+            activation=activations[0],
+            kernel_regularizer=l2,
+            input_shape=(nx,),
+        )
+    )
+
+    if len(layers) > 1:
+        model.add(K.layers.Dropout(rate=keep_prob))
+
+    for i in range(1, len(layers)):
         model.add(
             K.layers.Dense(
-                units=layers[i] + 1,
+                units=layers[i],
                 activation=activations[i],
-                kernel_regularizer=K.regularizers.l2(lambtha),
+                kernel_regularizer=l2,
             )
         )
+
         if i < len(layers) - 1:
             model.add(K.layers.Dropout(rate=keep_prob))
 
-    model.compile(optimizer="adam", loss="mean_squared_error")
+    # model.compile(optimizer="adam", loss="mean_squared_error")
 
     return model
