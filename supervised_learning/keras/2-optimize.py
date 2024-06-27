aaa0acee2e1ba98895f005dbb10b893cc6190ef6
root <root@DESKTOP-FADRK94>
2024-06-26 19:11:08 -0500

    Ok maybe this will go through at least?

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -8,9 +8,8 @@ import tensorflow.keras as K
 def optimize_model(network, alpha, beta1, beta2):
     """Documentation"""
 
-    print(f"Alpha: {alpha}, Beta1: {beta1}, Beta2: {beta2}")
     adam_optomizer = K.optimizers.Adam(
-        lr=alpha,
+        learning_rate=alpha,
         beta_1=beta1,
         beta_2=beta2)
 
--- supervised_learning/keras/3-one_hot.py
+++ supervised_learning/keras/3-one_hot.py
@@ -6,5 +6,11 @@ import tensorflow.keras as K
 
 
 def one_hot(labels, classes=None):
-    """Documentation"""
-    return None
+    if classes is not None:
+        class_num = classes
+    else:
+        classes = np.max(labels) + 1
+
+    matrix = K.utils.to_categorical(labels, class_num)
+
+    return matrix
