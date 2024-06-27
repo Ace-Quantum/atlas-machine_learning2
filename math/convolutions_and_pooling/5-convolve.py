e17ef57ad02b8b7f35b7a030d9d2e567e90cbdaf
root <root@DESKTOP-FADRK94>
2024-06-27 17:29:05 -0500

    Crazy printf and airbnb2 comes on the same weeks now

--- math/convolutions_and_pooling/5-convolve.py
+++ math/convolutions_and_pooling/5-convolve.py
@@ -5,5 +5,43 @@
 import numpy as np
 
 
-def convolve_grayscale_valid(images, kernel):
+def convolve(images, kernels, padding='same', stride=(1, 1)):
     """Documentation"""
+
+    m, h, w, c = images.shape
+    kh, kw, kc, nc = kernels.shape
+    sh, sw = stride
+
+    if padding == 'same':
+        pad_h = (sh * (h - 1) + kh - h) // 2
+        pad_w = (sw * (w - 1) + kw - w) // 2
+    elif padding == 'valid':
+        pad_h, pad_w = 0, 0
+    else:
+        pad_h, pad_w = padding
+
+    pad_images = np.pad(
+        images, ((0, 0), (pad_h, pad_h),
+                 (pad_w, pad_w), (0, 0)), mode="constant"
+    )
+
+    output_h = (h + 2 * pad_h - kh) // sh + 1
+    output_w = (w + 2 * pad_w - kw) // sw + 1
+
+    con_images = np.zeros((m, output_h, output_w, nc))
+
+    for k in range(nc):
+        for i in range(output_h):
+            for j in range(output_w):
+
+                st_i = i * sh
+                st_j = j * sw
+                end_i = st_i + kh
+                end_j = st_j + kw
+
+                con_images[:, i, j, k] = np.sum(
+                    pad_images[:, st_i:end_i, st_j:end_j, :] * kernels[
+                        :, :, k], axis=(1, 2, 3)
+                )
+
+    return con_images
