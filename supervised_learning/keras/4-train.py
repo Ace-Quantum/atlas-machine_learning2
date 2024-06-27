7006b4f909c8d87b6753dc36a994e8ee06bbf927
root <root@DESKTOP-FADRK94>
2024-06-26 19:16:53 -0500

    There's no actual way that works

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -9,4 +9,10 @@ def train_model(network, data, labels,
                 batch_size, epochs, verbose=True,
                 shuffle=False):
     """Documentation"""
-    return None
+    
+    model = network
+    history = model.fit(data, labels,
+                batch_size=batch_size, epochs=epochs,
+                verbose=verbose, shuffle=shuffle)
+    
+    return history
