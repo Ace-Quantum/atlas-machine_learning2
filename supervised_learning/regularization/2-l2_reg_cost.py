bef5e31171d03ea13e58c878d4c8d7ccabdb325f
root <root@DESKTOP-FADRK94>
2024-06-27 15:46:52 -0500

    Ok let's make the grade

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -6,12 +6,14 @@ import tensorflow as tf
 def l2_reg_cost(cost, model):
     """And some more documentation"""
     l2_loss = tf.constant(0.0)
+    layer_losses = []
 
     for layer in model.layers:
         if layer.__class__.__name__ == 'Dense':
-            l2_loss += tf.reduce_sum(tf.square(layer.kernel))
+            l2_loss = tf.reduce_sum(tf.square(layer.kernel))
             l2_loss += tf.reduce_sum(tf.square(layer.bias))
+            layer_losses.append(l2_loss)
 
-    total_cost = cost + l2_loss
+    total_cost = cost + tf.stack(layer_losses)
 
     return total_cost
