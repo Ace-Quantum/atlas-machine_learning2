310f8dab8227931922ab845c8dbb13d4b7e0d3e9
root <root@DESKTOP-FADRK94>
2024-06-26 19:23:00 -0500

    Well, the rest of the train functions won't work either. There's not too much of a point other than to set tomorrow's ace up

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -5,14 +5,18 @@
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels,
-                batch_size, epochs, verbose=True,
-                shuffle=False):
+def train_model(network, data, labels, batch_size, epochs,
+                verbose=True, shuffle=False):
     """Documentation"""
-    
+
     model = network
-    history = model.fit(data, labels,
-                batch_size=batch_size, epochs=epochs,
-                verbose=verbose, shuffle=shuffle)
-    
+    history = model.fit(
+        data,
+        labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle,
+    )
+
     return history
