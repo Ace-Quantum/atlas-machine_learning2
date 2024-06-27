2008e07fbcfba29a5219da27371b973a8e262892
root <root@DESKTOP-FADRK94>
2024-06-26 21:53:47 -0500

    task 0 test 4

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -8,21 +8,18 @@ import numpy as np
 def convolve_grayscale_valid(images, kernel):
     """Documentation"""
 
-    m, h, w = images.shape
+    pad_height = int(np.ceil((images.shape[1] - kernel.shape[0]) / 2))
+    pad_width = int(np.ceil((images.shape[2] - kernel.shape[1]) / 2))
+
+    padded_images = np.pad(images, ((0,0), (pad_height, pad_height), (pad_width, pad_width)), mode='constant')
+
+    m, _, = images.shape
     kh, kw = kernel.shape
-
-    out_h = h - kh + 1
-    out_w = w - kw + 1
-
-    print(f"Images shape: {images.shape}")
-    print(f"Kernel shape: {kernel.shape}")
-    print(f"Output shape: (m, {out_h}, {out_w})")
-
-    output = np.zeros((m, out_h, out_w))
+    output = np.zeros((m, padded_images.shape[1] - kh + 1, padded_images.shape[2] - kw + 1))
 
     for i in range(m):
-        for y in range(out_h):
-            for x in range(out_w):
-                output[i, y, x] = np.sum(images[i, y:y+kh, x:x+kw] * kernel)
+        for y in range(padded_images.shape[2] - kh + 1):
+            for x in range(padded_images.shape[y] - kw + 1):
+                output[i, y, x] = np.sum(padded_images[i, y:y+kh, x:x+kw] * kernel)
 
     return output
