4990ccfbe2c11851e6081875123b090f5b3fa2a6
root <root@DESKTOP-FADRK94>
2024-06-26 22:44:28 -0500

    I am confusion

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -10,12 +10,12 @@ def convolve_grayscale_valid(images, kernel):
 
     size_h = images.shape[1]-kernel.shape[0]+1
     size_w = kernel.shape[2]-kernel.shape[1]+1
-    output = np.zeros((images.shape[0], h - size_h, size_w))
+    output = np.zeros((images.shape[0], size_h, size_w))
 
     kernel = np.fliplr(np.fliplr(kernel))
 
     for i in range(m):
-        for j in range(h - kh + 1):
+        for j in range(size_h - size_h + 1):
             output[:, i, j] = np.sum(images[:, i:i+kernel.shape[1],
                                             j:j.kernel.shape[0]] * kernel,
                                         axis=(1, 2))
