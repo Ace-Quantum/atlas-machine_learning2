24e3e1129dfdfc0840307b8f0f954cbe53864126
root <root@DESKTOP-FADRK94>
2024-06-27 10:20:17 -0500

    Oh Python

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -25,7 +25,7 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     )
 
     if len(layers) > 1:
-        model.add(K.layers.Dropout(rate=keep_prob))
+        model.add(K.layers.Dropout(rate=1 - keep_prob))
 
     for i in range(1, len(layers)):
         model.add(
@@ -37,7 +37,7 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
         )
 
         if i < len(layers) - 1:
-            model.add(K.layers.Dropout(rate=keep_prob))
+            model.add(K.layers.Dropout(rate=1 - keep_prob))
 
     # model.compile(optimizer="adam", loss="mean_squared_error")
 
