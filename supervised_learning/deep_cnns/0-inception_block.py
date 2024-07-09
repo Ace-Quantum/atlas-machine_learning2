84d8bdcc6c40e60b0a18b5763fbb3b3ba7287da0
root <root@DESKTOP-FADRK94>
2024-07-09 15:31:10 -0500

    I think this is it???

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -14,40 +14,45 @@ def inception_block(A_prev, filters):
         activation="relu"
     )(A_prev)
 
-    # The resource has us redeclare layer_1. Why?
-    # Also!!!!
-    # What the HECK does it mean putting the little dealiboop at the end?
-    layer_1 = K.layers.Conv2D(
-        filters=filters[1],
-        kernel_size=(1, 1),
-        padding="same",
-        activation="relu"
-    )(layer_1)
-
     layer_2 = K.layers.Conv2D(
-        filters=filters[2],
-        kernel_size=(3, 3),
-        padding="same",
-        activation="relu"
+        filters=filters[1],
+        kernel_size=(1,1),
+        padding='same',
+        activation='relu'
     )(A_prev)
 
     layer_2 = K.layers.Conv2D(
-        filters=filters[3],
-        kernel_size=(1, 1),
-        padding="same",
-        activation="relu"
+        filters=filters[2],
+        kernel_size=(3,3),
+        padding='same',
+        activation='relu'
     )(layer_2)
 
-    layer_3 = K.layers.MaxPooling2D(
-        pool_size=(3, 3),
-        strides=(1, 1),
-        padding="same")(A_prev)
+    layer_3 = K.layers.Conv2D(
+        filters=filters[3],
+        kernel_size=(1,1),
+        padding='same',
+        activation='relu'
+    )(A_prev)
 
     layer_3 = K.layers.Conv2D(
-        filters=filters[5],
-        kernel_size=(1, 2),
-        padding="same",
-        activation="relu"
+        filters=filters[4],
+        kernel_size=(5,5),
+        padding='same',
+        activation='relu'
     )(layer_3)
 
-    return K.layers.concatenate([layer_1, layer_2, layer_3], axis=3)
+    layer_4 = K.layers.MaxPooling2D(
+        pool_size=(3,3),
+        strides=(1,1),
+        padding='same'
+    )(A_prev)
+
+    layer_4 = K.layers.Conv2D(
+        filters=filters[5],
+        kernel_size=(1,1),
+        padding='same',
+        activation='relu'
+    )(layer_4)
+
+    return K.layers.concatenate([layer_1, layer_2, layer_3, layer_4], axis=3)
