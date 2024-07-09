4c83026ddab60ee60a4908a4e22864c64b013c08
root <root@DESKTOP-FADRK94>
2024-07-09 12:59:36 -0500

    Maybe it'll run on the checker's end?

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -6,3 +6,12 @@ from tensorflow import keras as K
 
 def inception_block(A_prev, filters):
     """Builds an inception block"""
+
+    layer_1 = K.layers.convolutional.Conv2D(
+        layers=filters[0],
+        kernel_size=(1, 1),
+        padding='same',
+        activation='relu'
+    )
+
+    return layer_1
