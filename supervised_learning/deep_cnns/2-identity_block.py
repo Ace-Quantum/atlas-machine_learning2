962d5880f1b40bfc5749bf028ba348bd6c678156
root <root@DESKTOP-FADRK94>
2024-07-10 17:28:43 -0500

    Looks like we got it!

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -6,3 +6,39 @@ from tensorflow import keras as K
 
 def identity_block(A_prev, filters):
     """Builds an identity block"""
+
+    input_shortcut = A_prev
+
+    layer = K.layers.Conv2D(
+        filters=filters[0],
+        kernel_size=(1, 1),
+        padding='same',
+        kernel_initializer = K.initializers.glorot_uniform(seed=0)
+    )(A_prev)
+
+    layer = K.layers.BatchNormalization(axis = 3)(layer)
+    layer = K.layers.Activation('relu')(layer)
+
+    layer = K.layers.Conv2D(
+        filters=filters[1],
+        kernel_size=(3, 3),
+        padding='same',
+        kernel_initializer = K.initializers.glorot_uniform(seed=0)
+    )(layer)
+
+    layer = K.layers.BatchNormalization(axis=3)(layer)
+    layer = K.layers.Activation('relu')(layer)
+
+    layer = K.layers.Conv2D(
+        filters=filters[2],
+        kernel_size=(1,1),
+        padding='same',
+        kernel_initializer = K.initializers.glorot_uniform(seed=0)
+    )(layer)
+
+    layer = K.layers.BatchNormalization(axis=3)(layer)
+
+    layer = K.layers.Add()([layer, input_shortcut])
+    layer = K.layers.Activation('relu')(layer)
+
+    return layer
