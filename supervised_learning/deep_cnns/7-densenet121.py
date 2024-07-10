d274e9ca8b5b53ea41ba68bd470ce62d607c0699
root <root@DESKTOP-FADRK94>
2024-07-10 14:47:53 -0500

    check for pycode

--- supervised_learning/deep_cnns/7-densenet121.py
+++ supervised_learning/deep_cnns/7-densenet121.py
@@ -2,6 +2,8 @@
 """Builds the DenseNet-121 architecture"""
 
 from tensorflow import keras as K
+dense_block = __import__('5-dense_block').dense_block
+transition_layer = __import__('6-transitional_layer').transitional_layer
 
 
 def densenet121(growth_rate=31, compression=1.0):
