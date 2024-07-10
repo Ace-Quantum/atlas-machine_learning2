40340422f94c4bd8e01b4141f376597fc6c3d16e
root <root@DESKTOP-FADRK94>
2024-07-10 17:58:51 -0500

    Each point is a point that counts

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -12,33 +12,33 @@ def identity_block(A_prev, filters):
     layer = K.layers.Conv2D(
         filters=filters[0],
         kernel_size=(1, 1),
-        padding='same',
-        kernel_initializer = K.initializers.glorot_uniform(seed=0)
+        padding="same",
+        kernel_initializer=K.initializers.glorot_uniform(seed=0),
     )(A_prev)
 
-    layer = K.layers.BatchNormalization(axis = 3)(layer)
-    layer = K.layers.Activation('relu')(layer)
+    layer = K.layers.BatchNormalization(axis=3)(layer)
+    layer = K.layers.Activation("relu")(layer)
 
     layer = K.layers.Conv2D(
         filters=filters[1],
         kernel_size=(3, 3),
-        padding='same',
-        kernel_initializer = K.initializers.glorot_uniform(seed=0)
+        padding="same",
+        kernel_initializer=K.initializers.glorot_uniform(seed=0),
     )(layer)
 
     layer = K.layers.BatchNormalization(axis=3)(layer)
-    layer = K.layers.Activation('relu')(layer)
+    layer = K.layers.Activation("relu")(layer)
 
     layer = K.layers.Conv2D(
         filters=filters[2],
-        kernel_size=(1,1),
-        padding='same',
-        kernel_initializer = K.initializers.glorot_uniform(seed=0)
+        kernel_size=(1, 1),
+        padding="same",
+        kernel_initializer=K.initializers.glorot_uniform(seed=0),
     )(layer)
 
     layer = K.layers.BatchNormalization(axis=3)(layer)
 
     layer = K.layers.Add()([layer, input_shortcut])
-    layer = K.layers.Activation('relu')(layer)
+    layer = K.layers.Activation("relu")(layer)
 
     return layer
