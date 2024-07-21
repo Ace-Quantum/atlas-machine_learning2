c2ad7167b6b5d99424d25fbc4349a817c62e86cf
root <root@DESKTOP-FADRK94>
2024-07-21 15:55:16 -0500

    Hey ace sending in your code before running the checker is a good idea

--- supervised_learning/cnn/4-lenet5.py
+++ supervised_learning/cnn/4-lenet5.py
@@ -6,31 +6,53 @@ tf.disable_v2_behavior()
 def lenet5(x, y):
     initializer = tf.keras.initializers.VarianceScaling(scale=2.0)
 
-    conv1 = tf.layers.conv2d(x, filters=6, kernel_size=(5, 5),
-                             padding='same', activation=tf.nn.relu, kernel_initializer=initializer)
+    conv1 = tf.nn.conv2d(x,
+                         filters=tf.Variable(initializer(shape=(5, 5, 1, 6))),
+                         strides=[1, 1, 1, 1],
+                         padding='SAME')
     
-    pool1 = tf.layers.max_pooling2d(conv1, pool_size=(2, 2), strides=(2, 2))
-
-    conv2 = tf.layers.conv2d(pool1, filters=16, kernel_size=(5, 5),
-                             padding='valid', activation=tf.nn.relu,
-                             kernel_initializer=initializer)
+    conv1 = tf.nn.relu(conv1)
     
-    pool2 = tf.layers.max_pooling2d(conv2, pool_size=(2, 2), strides=(2, 2))
+    pool1 = tf.nn.max_pool(conv1,
+                           ksize=[1, 2, 2, 1],
+                           strides=[1, 2, 2, 1],
+                           padding='VALID')
+    
+    conv2 = tf.nn.conv2d(pool1,
+                         filters=tf.Variable(initializer(shape=(5, 5, 6, 16))),
+                         strides=[1, 1, 1, 1],
+                         padding='VALID')
+    
+    conv2 = tf.nn.relu(conv2)
 
-    flat = tf.layers.flatten(pool2)
+    pool2 = tf.nn.max_pool(conv2,
+                           ksize=[1, 2, 2, 1],
+                           strides=[1, 2, 2, 1],
+                           padding='VALID')
+    
+    flat_shape = pool2.get_shape().as_list()
+    flat = tf.reshape(pool2, [-1, flat_shape[1] * flat_shape[2] * flat_shape[3]])
 
-    fc1 = tf.layers.dense(flat, units=120, activation=tf.relu, kernel_initializer=initializer)
+    # Dense Layer, 120 nodes
+    fc1_weights = tf.Variable(initializer([flat_shape[1] * flat_shape[2] * flat_shape[3], 120]))
+    fc1_biases = tf.Variable(tf.zeros([120]))
+    fc1 = tf.nn.relu(tf.matmul(flat, fc1_weights) + fc1_biases)
 
-    fc2 = tf.layers.dense(fc1, units=84, activation=tf.nn.relu, kernel_initializer=initializer)
-
-    logits = tf.layers.dense(fc2, units=10, kernel_initialzer=initializer)
+    # dense layer, 84 nodes
+    fc2_weights = tf.Variable(initializer([120, 84]))
+    fc2_biases = tf.Variable(tf.zeros([84]))
+    fc2 = tf.nn.relu(tf.matmul(fc1, fc2_weights) + fc2_biases)
+    
+    output_weights = tf.Variable(initializer([84, 10]))
+    output_biases = tf.Variable(tf.zeros([10]))
+    logits = tf.matmul(fc2, output_weights) + output_biases
     softmax_output = tf.nn.softmax(logits)
 
     loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=y))
 
     train_op = tf.train.AdamOptimizer().minimize(loss)
 
-    correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(y, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    correct_pred = tf.equal(tf.argmax(logits, 1), tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
     return softmax_output, train_op, loss, accuracy
