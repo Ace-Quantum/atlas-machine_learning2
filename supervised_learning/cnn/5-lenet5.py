f1357b714cc8c99a2c7881c9f0a47af90aa67305
root <root@DESKTOP-FADRK94>
2024-07-21 16:20:25 -0500

    At least programming offers a hyper focus for dealing with problems

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -1,33 +1,40 @@
 #!/usr/bin/env python3
 
+"""Documentation"""
+
 from tensorflow import keras as K
-from tensorflow.keras import layers, models, initializers
+
 
 def lenet5(X):
-    initializer = initializers.he_normal(seed=0)
+    """Documentation"""
+    initializer = K.initializers.he_normal(seed=0)
 
-    conv1 = layers.Conv2D(6, (5, 5), padding='same',
-                          kernel_initializer=initializer, activation='relu')(X)
-    
-    pool1 = layers.MaxPooling2D((2, 2), strides=(2, 2))(conv1)
+    conv1 = K.layers.Conv2D(6, (5, 5),
+                            padding="same", kernel_initializer=initializer,
+                            activation="relu")(X)
 
-    conv2 = layers.Conv2D(16, (5, 5), padding='valid',
-                          kernel_initializer=initializer, activation='relu')(pool1)
-    
-    pool2 = layers.MaxPooling2D((2, 2), strides=(2,2))(conv2)
+    pool1 = K.layers.MaxPooling2D((2, 2), strides=(2, 2))(conv1)
 
-    flat = layers.Flatten()(pool2)
+    conv2 = K.layers.Conv2D(
+        16, (5, 5), padding="valid", kernel_initializer=initializer,
+        activation="relu")(pool1)
 
-    fc1 = layers.Dense(120, kernel_initializer=initializer,
-                       activation='relu')(flat)
-    
-    fc2 = layers.Dense(84, kernel_initializer=initializer,
-                       activation='relu')(fc1)
-    
-    output = layers.Dense(10, kernel_initializer=initializer,
-                          activation='softmax')(fc2)
-    
-    model = models.Model(inputs=X, outputs=output)
+    pool2 = K.layers.MaxPooling2D((2, 2), strides=(2, 2))(conv2)
 
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    flat = K.layers.Flatten()(pool2)
+
+    fc1 = K.layers.Dense(120, kernel_initializer=initializer,
+                         activation="relu")(flat)
+
+    fc2 = K.layers.Dense(84, kernel_initializer=initializer,
+                         activation="relu")(fc1)
+
+    output = K.layers.Dense(10, kernel_initializer=initializer,
+                            activation="softmax")(fc2)
+
+    model = K.models.Model(inputs=X, outputs=output)
+
+    model.compile(
+        optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"]
+    )
     return model
