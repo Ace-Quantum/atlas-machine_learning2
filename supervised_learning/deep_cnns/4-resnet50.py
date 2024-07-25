8072d9f5260b36394241df9957c62a35c9d04075
root <root@DESKTOP-FADRK94>
2024-07-25 14:59:19 -0500

    Ok finally

--- supervised_learning/deep_cnns/4-resnet50.py
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -3,8 +3,9 @@
 
 from tensorflow import keras as K
 
-identity_block = __import__('2-identity_block').identity_block
-projection_block = __import__('3-projection_block').projection_block
+identity_block = __import__("2-identity_block").identity_block
+projection_block = __import__("3-projection_block").projection_block
+
 
 def resnet50():
     """Builds the ResNet-50 architecture"""
@@ -14,20 +15,18 @@ def resnet50():
     # Start of stage 1
     layers = K.layers.Conv2D(
         kernel_size=7,
-        padding='same',
+        padding="same",
         filters=64,
         strides=2,
-        kernel_initializer=K.initializers.he_normal()
+        kernel_initializer=K.initializers.he_normal(),
     )(model_start)
 
     layers = K.layers.BatchNormalization(axis=3)(layers)
-    layers = K.layers.Activation('relu')(layers)
+    layers = K.layers.Activation("relu")(layers)
 
-    layers = K.layers.MaxPooling2D(
-        pool_size=3,
-        strides=2,
-        padding='same'
-    )(layers)
+    layers = K.layers.MaxPooling2D(pool_size=3,
+                                   strides=2,
+                                   padding="same")(layers)
 
     # Start of stage 2
     layers = projection_block(layers, [64, 64, 256], 1)
@@ -55,15 +54,16 @@ def resnet50():
 
     # last stage
     # Need to find out why the parameters are this way
-    layers = K.layers.AveragePooling2D(
-        pool_size=7,
-        strides=1,
-        padding='valid'
-    )(layers)
+    layers = K.layers.AveragePooling2D(pool_size=7,
+                                       strides=1,
+                                       padding="valid")(layers)
 
     # layers = K.layers.Flatten()(layers)
 
-    layers = K.layers.Dense(1000, activation='softmax', kernel_initializer=K.initializers.he_normal())(layers)
+    layers = K.layers.Dense(
+        1000, activation="softmax",
+        kernel_initializer=K.initializers.he_normal()
+    )(layers)
 
     model = K.models.Model(model_start, layers)
 
